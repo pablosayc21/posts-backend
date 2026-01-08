8e1659476f7999163c9dc576a9c8ccdc8db94f68
pablosayc21 <pablosayc@gmail.com>
2026-01-07 21:34:34 -0600

    fix(errores): corrige el mensaje de error para que no salga siempre 500

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -1,29 +1,14 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus} from '@nestjs/common';
-import { Response } from 'express';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
 import { ApiResponse } from '../dto/response.dto';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
-    catch(exception: unknown, host: ArgumentsHost) {
-
+    catch(exception: HttpException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
-        const response = ctx.getResponse<Response>();
-
-        let status = HttpStatus.INTERNAL_SERVER_ERROR;
-        let message = 'Error interno del sistema';
-        let errors: any[] = [];
-
-        if (exception instanceof HttpException) {
-            status = exception.getStatus();
-            const res = exception.getResponse();
-            if (typeof res === 'string') {
-                message = res;
-            } else {
-                message = (res as any).message || message;
-                errors = Array.isArray((res as any).message) ? (res as any).message : [];
-            }
-        }
-
+        const response = ctx.getResponse();
+        const status = exception.getStatus();
+        const exceptionResponse = exception.getResponse();
+        const message = typeof exceptionResponse === 'string' ? exceptionResponse : (exceptionResponse as any).message;
         response.status(status).json(ApiResponse.error(message, status));
     }
 }
