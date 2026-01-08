61d6f8e94e43ebffad468662dcbfc62460e5f5ab
pablosayc21 <pablosayc@gmail.com>
2026-01-07 18:46:58 -0600

    feat(posts): agrega endpoints de listado y de creación

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { Param } from '@nestjs/common';
-
+import { CreatePostDto } from './dto/post.dto';
 @Controller('posts')
 export class PostsController {
 
@@ -9,11 +9,19 @@ export class PostsController {
 
     }
 
+    //Plural
     @Get()
     async findAll(){
         return this.postService.findAll();
     }
 
+    @Post()
+    async create(@Body() createPostDto: CreatePostDto) {
+        return this.postService.create(createPostDto);
+    }
+
+
+    //Singular
     @Get(':id')
     async findOne(@Param('id') id: string) {
         return this.postService.findOne(id);
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Post } from './schemas/post.schema';
 import { Model } from 'mongoose';
-
+import { CreatePostDto } from './dto/post.dto';
 @Injectable()
 export class PostsService {
 
@@ -10,6 +10,11 @@ export class PostsService {
         @InjectModel(Post.name) private postModel: Model<Post>,
     ) {
 
+    }
+
+    async create(createPostDto: CreatePostDto) {
+        const post = new this.postModel(createPostDto);
+        return post.save();
     }
 
     async findAll() {
